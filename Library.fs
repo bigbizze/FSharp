9483479f66161de622dafc49e12476e15bb3ea9b
Charles Anderson <hpcngmoh@gmail.com>
2021-01-25 20:14:24 -0500

    Add project files.

---
+++ Library.fs
@@ -0,0 +1,9 @@
+﻿namespace FSharp
+open UnityEngine;
+
+type Basic() =
+    inherit MonoBehaviour()
+    member this.Start() = Debug.Log("Bitch")
+//module Say =
+//    let hello name =
+//        printfn "Hello %s" name
